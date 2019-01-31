73ad1f20868fae3d69b5cd2bd02a29243540d598
ScottLiao920 <35758766+ScottLiao920@users.noreply.github.com>
2019-01-31 13:50:21 +0800

    Add files via upload
    merged eval with train
    printout running loss every 100 steps and then eval loss  for 10 steps

--- textsum/seq2seq_attention.py
+++ textsum/seq2seq_attention.py
@@ -14,12 +14,9 @@
 # ==============================================================================
 
 """Trains a seq2seq model.
-
 WORK IN PROGRESS.
-
 Implement "Abstractive Text Summarization using Sequence-to-sequence RNNS and
 Beyond."
-
 """
 import sys
 import time
@@ -66,148 +63,167 @@ tf.app.flags.DEFINE_integer('random_seed', 111, 'A seed value for randomness.')
 
 
 def _RunningAvgLoss(loss, running_avg_loss, summary_writer, step, decay=0.999):
-  """Calculate the running average of losses."""
-  if running_avg_loss == 0:
-    running_avg_loss = loss
-  else:
-    running_avg_loss = running_avg_loss * decay + (1 - decay) * loss
-  running_avg_loss = min(running_avg_loss, 12)
-  loss_sum = tf.Summary()
-  loss_sum.value.add(tag='running_avg_loss', simple_value=running_avg_loss)
-  summary_writer.add_summary(loss_sum, step)
-  sys.stdout.write('running_avg_loss: %f\n' % running_avg_loss)
-  return running_avg_loss
-
-
-def _Train(model, data_batcher):
-  """Runs model training."""
-  with tf.device('/cpu:0'):
-    model.build_graph()
-    saver = tf.train.Saver()
-    # Train dir is different from log_root to avoid summary directory
-    # conflict with Supervisor.
-    summary_writer = tf.summary.FileWriter(FLAGS.train_dir)
-    sv = tf.train.Supervisor(logdir=FLAGS.log_root,
-                             is_chief=True,
-                             saver=saver,
-                             summary_op=None,
-                             save_summaries_secs=60,
-                             save_model_secs=FLAGS.checkpoint_secs,
-                             global_step=model.global_step)
-    sess = sv.prepare_or_wait_for_session(config=tf.ConfigProto(
-        allow_soft_placement=True))
-    running_avg_loss = 0
-    step = 0
-    while not sv.should_stop() and step < FLAGS.max_run_steps:
-      (article_batch, abstract_batch, targets, article_lens, abstract_lens,
-       loss_weights, _, _) = data_batcher.NextBatch()
-      (_, summaries, loss, train_step) = model.run_train_step(
-          sess, article_batch, abstract_batch, targets, article_lens,
-          abstract_lens, loss_weights)
-
-      summary_writer.add_summary(summaries, train_step)
-      running_avg_loss = _RunningAvgLoss(
-          running_avg_loss, loss, summary_writer, train_step)
-      step += 1
-      if step % 100 == 0:
-        summary_writer.flush()
-    sv.Stop()
+    """Calculate the running average of losses."""
+    if running_avg_loss == 0:
+        running_avg_loss = loss
+    else:
+        running_avg_loss = running_avg_loss * decay + (1 - decay) * loss
+    running_avg_loss = min(running_avg_loss, 12)
+    loss_sum = tf.Summary()
+    loss_sum.value.add(tag='running_avg_loss', simple_value=running_avg_loss)
+    summary_writer.add_summary(loss_sum, step)
+    #sys.stdout.write('running_avg_loss: %f\n' % running_avg_loss)
     return running_avg_loss
 
 
-def _Eval(model, data_batcher, vocab=None):
-  """Runs model eval."""
-  model.build_graph()
-  saver = tf.train.Saver()
-  summary_writer = tf.summary.FileWriter(FLAGS.eval_dir)
-  sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-  running_avg_loss = 0
-  step = 0
-  while True:
-    time.sleep(FLAGS.eval_interval_secs)
-    try:
-      ckpt_state = tf.train.get_checkpoint_state(FLAGS.log_root)
-    except tf.errors.OutOfRangeError as e:
-      tf.logging.error('Cannot restore checkpoint: %s', e)
-      continue
+def _Train(model, eval_model, data_batcher, vocab=None):
+    """Runs model training."""
+    with tf.device('/cpu:0'):
+        model.build_graph()
+        saver = tf.train.Saver()
+        # Train dir is different from log_root to avoid summary directory
+        # conflict with Supervisor.
+        summary_writer = tf.summary.FileWriter(FLAGS.train_dir)
+        sv = tf.train.Supervisor(logdir=FLAGS.log_root,
+                                 is_chief=True,
+                                 saver=saver,
+                                 summary_op=None,
+                                 save_summaries_secs=60,
+                                 save_model_secs=FLAGS.checkpoint_secs,
+                                 global_step=model.global_step)
+        sess = sv.prepare_or_wait_for_session(config=tf.ConfigProto(
+            allow_soft_placement=True))
+        running_avg_loss = 0
+        step = 0
+        while not sv.should_stop() and step < FLAGS.max_run_steps:
+            (article_batch, abstract_batch, targets, article_lens, abstract_lens,
+             loss_weights, _, _) = data_batcher.NextBatch()
+            (_, summaries, loss, train_step) = model.run_train_step(
+                sess, article_batch, abstract_batch, targets, article_lens,
+                abstract_lens, loss_weights)
 
-    if not (ckpt_state and ckpt_state.model_checkpoint_path):
-      tf.logging.info('No model to eval yet at %s', FLAGS.train_dir)
-      continue
+            summary_writer.add_summary(summaries, train_step)
+            running_avg_loss = _RunningAvgLoss(
+                running_avg_loss, loss, summary_writer, train_step)
+            step += 1
+            if step % 100 == 0:
+                summary_writer.flush()
+                sys.stdout.write("running_avg_loss: %f\n"%running_avg_loss)
+                _Eval(model, batcher, 10, vocab=vocab,)
+        sv.Stop()
+        return running_avg_loss
 
-    tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
-    saver.restore(sess, ckpt_state.model_checkpoint_path)
 
-    (article_batch, abstract_batch, targets, article_lens, abstract_lens,
-     loss_weights, _, _) = data_batcher.NextBatch()
-    (summaries, loss, train_step) = model.run_eval_step(
-        sess, article_batch, abstract_batch, targets, article_lens,
-        abstract_lens, loss_weights)
-    tf.logging.info(
-        'article:  %s',
-        ' '.join(data.Ids2Words(article_batch[0][:].tolist(), vocab)))
-    tf.logging.info(
-        'abstract: %s',
-        ' '.join(data.Ids2Words(abstract_batch[0][:].tolist(), vocab)))
+def _Eval(model, data_batcher, iteration, vocab=None,):
+    """Runs model eval."""
+    model.build_graph()
+    saver = tf.train.Saver()
+    summary_writer = tf.summary.FileWriter(FLAGS.eval_dir)
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    running_avg_loss = 0
+    step = 0
+    while iteration > step:
+        time.sleep(FLAGS.eval_interval_secs)
+        try:
+            ckpt_state = tf.train.get_checkpoint_state(FLAGS.log_root)
+        except tf.errors.OutOfRangeError as e:
+            tf.logging.error('Cannot restore checkpoint: %s', e)
+            continue
 
-    summary_writer.add_summary(summaries, train_step)
-    running_avg_loss = _RunningAvgLoss(
-        running_avg_loss, loss, summary_writer, train_step)
-    if step % 100 == 0:
-      summary_writer.flush()
+        if not (ckpt_state and ckpt_state.model_checkpoint_path):
+            tf.logging.info('No model to eval yet at %s', FLAGS.train_dir)
+            continue
+
+        tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
+        saver.restore(sess, ckpt_state.model_checkpoint_path)
+
+        (article_batch, abstract_batch, targets, article_lens, abstract_lens,
+         loss_weights, _, _) = data_batcher.NextBatch()
+        (summaries, loss, train_step) = model.run_eval_step(
+            sess, article_batch, abstract_batch, targets, article_lens,
+            abstract_lens, loss_weights)
+        tf.logging.info(
+            'article:  %s',
+            ' '.join(data.Ids2Words(article_batch[0][:].tolist(), vocab)))
+        tf.logging.info(
+            'abstract: %s',
+            ' '.join(data.Ids2Words(abstract_batch[0][:].tolist(), vocab)))
+
+        summary_writer.add_summary(summaries, train_step)
+        running_avg_loss = _RunningAvgLoss(
+            running_avg_loss, loss, summary_writer, train_step)
+        if step % 10 == 0:
+            summary_writer.flush()
+            sys.stdout.write("eval_avg_loss: %f\n\n\n"%running_avg_loss)
 
 
 def main(unused_argv):
-  vocab = data.Vocab(FLAGS.vocab_path, 1000000)
-  # Check for presence of required special tokens.
-  assert vocab.CheckVocab(data.PAD_TOKEN) > 0
-  assert vocab.CheckVocab(data.UNKNOWN_TOKEN) >= 0
-  assert vocab.CheckVocab(data.SENTENCE_START) > 0
-  assert vocab.CheckVocab(data.SENTENCE_END) > 0
+    vocab = data.Vocab(FLAGS.vocab_path, 1000000)
+    # Check for presence of required special tokens.
+    assert vocab.CheckVocab(data.PAD_TOKEN) > 0
+    assert vocab.CheckVocab(data.UNKNOWN_TOKEN) >= 0
+    assert vocab.CheckVocab(data.SENTENCE_START) > 0
+    assert vocab.CheckVocab(data.SENTENCE_END) > 0
 
-  batch_size = 4
-  if FLAGS.mode == 'decode':
-    batch_size = FLAGS.beam_size
+    batch_size = 64
+    if FLAGS.mode == 'decode':
+        batch_size = FLAGS.beam_size
 
-  hps = seq2seq_attention_model.HParams(
-      mode=FLAGS.mode,  # train, eval, decode
-      min_lr=0.01,  # min learning rate.
-      lr=0.2,  # learning rate
-      batch_size=batch_size,
-      enc_layers=4,
-      enc_timesteps=120,
-      dec_timesteps=30,
-      min_input_len=2,  # discard articles/summaries < than this
-      num_hidden=256,  # for rnn cell
-      emb_dim=128,  # If 0, don't use embedding
-      max_grad_norm=2,
-      num_softmax_samples=4096)  # If 0, no sampled softmax.
+    hps = seq2seq_attention_model.HParams(
+        mode=FLAGS.mode,  # train, eval, decode
+        min_lr=0.01,  # min learning rate.
+        lr=0.15,  # learning rate
+        batch_size=batch_size,
+        enc_layers=4,
+        enc_timesteps=120,
+        dec_timesteps=30,
+        min_input_len=2,  # discard articles/summaries < than this
+        num_hidden=256,  # for rnn cell
+        emb_dim=128,  # If 0, don't use embedding
+        max_grad_norm=2,
+        num_softmax_samples=4096)  # If 0, no sampled softmax.
 
-  batcher = batch_reader.Batcher(
-      FLAGS.data_path, vocab, hps, FLAGS.article_key,
-      FLAGS.abstract_key, FLAGS.max_article_sentences,
-      FLAGS.max_abstract_sentences, bucketing=FLAGS.use_bucketing,
-      truncate_input=FLAGS.truncate_input)
-  tf.set_random_seed(FLAGS.random_seed)
+    eval_hps = seq2seq_attention_model.HParams(
+        mode='eval',  # train, eval, decode
+        min_lr=0.01,  # min learning rate.
+        lr=0.15,  # learning rate
+        batch_size=batch_size,
+        enc_layers=4,
+        enc_timesteps=120,
+        dec_timesteps=30,
+        min_input_len=2,  # discard articles/summaries < than this
+        num_hidden=256,  # for rnn cell
+        emb_dim=128,  # If 0, don't use embedding
+        max_grad_norm=2,
+        num_softmax_samples=4096)  # If 0, no sampled softmax.
 
-  if hps.mode == 'train':
-    model = seq2seq_attention_model.Seq2SeqAttentionModel(
-        hps, vocab, num_gpus=FLAGS.num_gpus)
-    _Train(model, batcher)
-  elif hps.mode == 'eval':
-    model = seq2seq_attention_model.Seq2SeqAttentionModel(
-        hps, vocab, num_gpus=FLAGS.num_gpus)
-    _Eval(model, batcher, vocab=vocab)
-  elif hps.mode == 'decode':
-    decode_mdl_hps = hps
-    # Only need to restore the 1st step and reuse it since
-    # we keep and feed in state for each step's output.
-    decode_mdl_hps = hps._replace(dec_timesteps=1)
-    model = seq2seq_attention_model.Seq2SeqAttentionModel(
-        decode_mdl_hps, vocab, num_gpus=FLAGS.num_gpus)
-    decoder = seq2seq_attention_decode.BSDecoder(model, batcher, hps, vocab)
-    decoder.DecodeLoop()
+    batcher = batch_reader.Batcher(
+        FLAGS.data_path, vocab, hps, FLAGS.article_key,
+        FLAGS.abstract_key, FLAGS.max_article_sentences,
+        FLAGS.max_abstract_sentences, bucketing=FLAGS.use_bucketing,
+        truncate_input=FLAGS.truncate_input)
+    tf.set_random_seed(FLAGS.random_seed)
+
+    if hps.mode == 'train':
+        model = seq2seq_attention_model.Seq2SeqAttentionModel(
+            hps, vocab, num_gpus=FLAGS.num_gpus)
+        eval_model = seq2seq_attention_model.Seq2SeqAttentionModel(
+            eval_hps, vocab, num_gpus=FLAGS.num_gpus)
+        _Train(model, eval_model, batcher, vocab=vocab)
+    elif hps.mode == 'eval':
+        model = seq2seq_attention_model.Seq2SeqAttentionModel(
+            hps, vocab, num_gpus=FLAGS.num_gpus)
+        _Eval(model, batcher, FLAGS.max_run_steps, vocab=vocab)
+    elif hps.mode == 'decode':
+        decode_mdl_hps = hps
+        # Only need to restore the 1st step and reuse it since
+        # we keep and feed in state for each step's output.
+        decode_mdl_hps = hps._replace(dec_timesteps=1)
+        model = seq2seq_attention_model.Seq2SeqAttentionModel(
+            decode_mdl_hps, vocab, num_gpus=FLAGS.num_gpus)
+        decoder = seq2seq_attention_decode.BSDecoder(model, batcher, hps, vocab)
+        decoder.DecodeLoop()
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
